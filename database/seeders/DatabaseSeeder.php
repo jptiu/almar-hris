831f9fc234a24c940d30d3b095b7ff5185dc22ef
Jhon Patrick Tiu <jhonpatrick.tiu@fligno.com>
2024-06-01 16:14:36 +0800

    added user roles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,16 +16,65 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
 
         \App\Models\User::factory()->create([
-            'name' => 'Admin Admin',
-            'email' => 'admin@admin.com',
+            'name' => 'Super Admin',
+            'email' => 'super@almarfinance.com',
             'email_verified_at' => now(),
             'password' => Hash::make('password'),
             'created_at' => now(),
             'updated_at' => now()
         ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Admin Admin',
+            'email' => 'admin@almarfinance.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        \App\Models\User::factory()->create([
+            'name' => 'HR Admin',
+            'email' => 'hr@almarfinance.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Branch Manager 1',
+            'email' => 'branch1@almarfinance.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Branch Manager 2',
+            'email' => 'branch2@almarfinance.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Loan Officer',
+            'email' => 'officer@almarfinance.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Auditor Admin',
+            'email' => 'auditor@almarfinance.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        // \App\Models\User::factory(10)->create();
 
         $this->call([
             PermissionsTableSeeder::class,
--- database/seeders/RolesTableSeeder.php
+++ database/seeders/RolesTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 namespace Database\Seeders;
+
 use App\Models\Role;
 use Illuminate\Database\Seeder;
 
@@ -7,20 +8,50 @@ class RolesTableSeeder extends Seeder
 {
     public function run()
     {
-        $roles = [[
-            'id'         => 1,
-            'title'      => 'Admin',
-            'created_at' => '2019-04-16 08:40:08',
-            'updated_at' => '2019-04-16 08:40:08',
-            'deleted_at' => null,
-        ],
+        $roles = [
             [
-                'id'         => 2,
-                'title'      => 'User',
+                'id' => 1,
+                'title' => 'Super Admin',
                 'created_at' => '2019-04-16 08:40:08',
                 'updated_at' => '2019-04-16 08:40:08',
                 'deleted_at' => null,
-            ]];
+            ],
+            [
+                'id' => 2,
+                'title' => 'Admin',
+                'created_at' => '2019-04-16 08:40:08',
+                'updated_at' => '2019-04-16 08:40:08',
+                'deleted_at' => null,
+            ],
+            [
+                'id'         => 3,
+                'title'      => 'HR',
+                'created_at' => '2019-04-16 08:40:08',
+                'updated_at' => '2019-04-16 08:40:08',
+                'deleted_at' => null,
+            ],
+            [
+                'id'         => 4,
+                'title'      => 'Branch Manager',
+                'created_at' => '2019-04-16 08:40:08',
+                'updated_at' => '2019-04-16 08:40:08',
+                'deleted_at' => null,
+            ],
+            [
+                'id'         => 5,
+                'title'      => 'Loan Officer',
+                'created_at' => '2019-04-16 08:40:08',
+                'updated_at' => '2019-04-16 08:40:08',
+                'deleted_at' => null,
+            ],
+            [
+                'id'         => 6,
+                'title'      => 'Auditor',
+                'created_at' => '2019-04-16 08:40:08',
+                'updated_at' => '2019-04-16 08:40:08',
+                'deleted_at' => null,
+            ]
+        ];
 
         Role::insert($roles);
     }
