936bf468dfbede80663b3f9445e85287e2ec60b8
Jhon Patrick Tiu <jhonpatrick.tiu@fligno.com>
2024-06-24 10:49:08 +0800

    added factory faker

--- database/seeders/DashboardTableSeeder.php
+++ database/seeders/DashboardTableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\DataFeed;
 use Illuminate\Support\Facades\Hash;
+use Faker\Factory;
 
 class DashboardTableSeeder extends Seeder
 {
