b392ec345fb0346fe2da138f909c466d499479d7
Christian James Simene <christianjamessimeneefiva@gmail.com>
2024-07-01 06:57:15 -0700

    Revert "Added Add Customer UI"
    This reverts commit c3aea9a5b1bced770353203a904ff9951975ee76.

--- resources/views/components/app/sidebar.blade.php
+++ resources/views/components/app/sidebar.blade.php
@@ -64,8 +64,23 @@
                                     </svg>
                                     <span class="text-sm font-medium ml-3 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Customer Profile</span>
                                 </div>
+                                <!-- Icon -->
+                                <div class="flex shrink-0 ml-2 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
+                                    <svg class="w-3 h-3 shrink-0 ml-1 fill-current text-slate-400 @if(in_array(Request::segment(1), ['customer'])){{ 'rotate-180' }}@endif" :class="open ? 'rotate-180' : 'rotate-0'" viewBox="0 0 12 12">
+                                        <path d="M5.9 11.4L.5 6l1.4-1.4 4 4 4-4L11.3 6z" />
+                                    </svg>
+                                </div>
                             </div>
                         </a>
+                        <div class="lg:hidden lg:sidebar-expanded:block 2xl:block">
+                            <ul class="pl-9 mt-1 @if(in_array(Request::segment(1), ['customer'])){{ 'bg-slate-900' }}@endif" x-data="{ open: {{ in_array(Request::segment(1), ['loan']) ? 1 : 0 }} }">
+                                <li class="mb-1 last:mb-0">
+                                    <a class="block text-slate-400 hover:text-slate-200 transition duration-150 truncate @if(Route::is('customer-add')){{ '!text-indigo-500' }}@endif" href="{{ route('customer.add') }}">
+                                        <span class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Add Customer</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
                     </li>
                     <!-- Grant Loan Entry -->
                     <li class="px-3 py-3 rounded-sm mb-0.5 last:mb-0 @if(in_array(Request::segment(1), ['loan'])){{ 'bg-slate-900' }}@endif" x-data="{ open: {{ in_array(Request::segment(1), ['loan']) ? 1 : 0 }} }">
