7e2b958c0203dd186d5b221cd1ca6edf811c814d
Jhon Patrick Tiu <jhonpatrick.tiu@fligno.com>
2024-07-01 20:30:06 +0800

    updates fixes

--- routes/web.php
+++ routes/web.php
@@ -45,47 +45,47 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
     Route::get('customer-daily', [CustomerController::class, 'daily'])->name('customer.daily');
     Route::get('customer-month', [CustomerController::class, 'month'])->name('customer.month');
     Route::get('customer', [CustomerController::class, 'index'])->name('customer.index');
-    Route::get('customer', [CustomerController::class, 'store'])->name('customer.store');
-    Route::get('customer', [CustomerController::class, 'update'])->name('customer.update');
-    Route::get('customer', [CustomerController::class, 'destroy'])->name('customer.destroy');
+    Route::get('customer/store', [CustomerController::class, 'store'])->name('customer.store');
+    Route::get('customer/update/{id}', [CustomerController::class, 'update'])->name('customer.update');
+    Route::get('customer/destroy/{id}', [CustomerController::class, 'destroy'])->name('customer.destroy');
 
     // Customer Type
     // Route::resource('customer-type', App\Http\Controllers\CustomerTypeController::class);
     Route::get('customer-type', [CustomerTypeController::class, 'index'])->name('customerType.index');
-    Route::get('customer-type', [CustomerTypeController::class, 'store'])->name('customerType.store');
-    Route::get('customer-type', [CustomerTypeController::class, 'update'])->name('customerType.update');
-    Route::get('customer-type', [CustomerTypeController::class, 'destroy'])->name('customerType.destroy');
+    Route::get('customer-type/store', [CustomerTypeController::class, 'store'])->name('customerType.store');
+    Route::get('customer-type/update/{id}', [CustomerTypeController::class, 'update'])->name('customerType.update');
+    Route::get('customer-type/destroy/{id}', [CustomerTypeController::class, 'destroy'])->name('customerType.destroy');
 
     // City/ or Town
     // Route::resource('city', App\Http\Controllers\CityTownController::class);
     Route::get('city', [CityTownController::class, 'index'])->name('city.index');
-    Route::get('city', [CityTownController::class, 'store'])->name('city.store');
-    Route::get('city', [CityTownController::class, 'update'])->name('city.update');
-    Route::get('city', [CityTownController::class, 'destroy'])->name('city.destroy');
+    Route::get('city/store', [CityTownController::class, 'store'])->name('city.store');
+    Route::get('city/update/{id}', [CityTownController::class, 'update'])->name('city.update');
+    Route::get('city/destroy/{id}', [CityTownController::class, 'destroy'])->name('city.destroy');
 
     // Loan
     Route::get('loan', [LoanController::class, 'index'])->name('loan.index');
-    Route::get('loan', [LoanController::class, 'store'])->name('loan.store');
-    Route::get('loan', [LoanController::class, 'update'])->name('loan.update');
-    Route::get('loan', [LoanController::class, 'destroy'])->name('loan.destroy');
+    Route::get('loan/store', [LoanController::class, 'store'])->name('loan.store');
+    Route::get('loan/update/{id}', [LoanController::class, 'update'])->name('loan.update');
+    Route::get('loan/destroy/{id}', [LoanController::class, 'destroy'])->name('loan.destroy');
 
     // Collection
     Route::get('collection', [CollectionController::class, 'index'])->name('collection.index');
-    Route::get('collection', [CollectionController::class, 'store'])->name('collection.store');
-    Route::get('collection', [CollectionController::class, 'update'])->name('collection.update');
-    Route::get('collection', [CollectionController::class, 'destroy'])->name('collection.destroy');
+    Route::get('collection/store', [CollectionController::class, 'store'])->name('collection.store');
+    Route::get('collection/update/{id}', [CollectionController::class, 'update'])->name('collection.update');
+    Route::get('collection/destroy/{id}', [CollectionController::class, 'destroy'])->name('collection.destroy');
 
     // Breakdown
     Route::get('breakdown', [BreakdownController::class, 'index'])->name('breakdown.index');
-    Route::get('breakdown', [BreakdownController::class, 'store'])->name('breakdown.store');
-    Route::get('breakdown', [BreakdownController::class, 'update'])->name('breakdown.update');
-    Route::get('breakdown', [BreakdownController::class, 'destroy'])->name('breakdown.destroy');
+    Route::get('breakdown/store', [BreakdownController::class, 'store'])->name('breakdown.store');
+    Route::get('breakdown/update/{id}', [BreakdownController::class, 'update'])->name('breakdown.update');
+    Route::get('breakdown/destroy/{id}', [BreakdownController::class, 'destroy'])->name('breakdown.destroy');
 
     // Expenses
     Route::get('expenses', [ExpensesController::class, 'index'])->name('expenses.index');
-    Route::get('expenses', [ExpensesController::class, 'store'])->name('expenses.store');
-    Route::get('expenses', [ExpensesController::class, 'update'])->name('expenses.update');
-    Route::get('expenses', [ExpensesController::class, 'destroy'])->name('expenses.destroy');
+    Route::get('expenses/store', [ExpensesController::class, 'store'])->name('expenses.store');
+    Route::get('expenses/update/{id}', [ExpensesController::class, 'update'])->name('expenses.update');
+    Route::get('expenses/destroy/{id}', [ExpensesController::class, 'destroy'])->name('expenses.destroy');
 
     // Route for the getting the data feed
     Route::get('/json-data-feed', [DataFeedController::class, 'getDataFeed'])->name('json_data_feed');
