5ce043f83111a8af60d2ddf13ad7211c41b351d9
Jhon Patrick Tiu <jhonpatrick.tiu@fligno.com>
2024-07-02 23:59:12 +0800

    added import csv

--- app/Http/Controllers/BarangayController.php
+++ app/Http/Controllers/BarangayController.php
@@ -116,4 +116,66 @@ class BarangayController extends Controller
 
         return redirect()->back()->with('success', 'Barangay deleted.');
     }
+
+    public function importCSV(Request $request)
+    {
+        $request->validate([
+            'import_csv' => 'required|mimes:csv',
+        ]);
+        //read csv file and skip data
+        $file = $request->file('import_csv');
+        $handle = fopen($file->path(), 'r');
+
+        //skip the header row
+        fgetcsv($handle);
+
+        $chunksize = 25;
+        while(!feof($handle))
+        {
+            $chunkdata = [];
+
+            for($i = 0; $i<$chunksize; $i++)
+            {
+                $data = fgetcsv($handle);
+                if($data === false)
+                {
+                    break;
+                }
+                $chunkdata[] = $data; 
+            }
+
+            $this->getchunkdata($chunkdata);
+        }
+        fclose($handle);
+
+        return redirect()->route('employee.create')->with('success', 'Data has been added successfully.');
+    }
+
+    public function getchunkdata($chunkdata)
+    {
+        foreach($chunkdata as $column){
+            $firstname = $column[0];
+            $lastname = $column[1];
+            $email = $column[2];
+            $phoneNumber = $column[3];
+            $dateOfBirth = $column[4];
+            $gender = $column[5];
+            $address = $column[6];
+            $skill = json_encode([$column[7]]);
+            $sallary = $column[8];
+
+            //create new employee
+            $employee = new Barangay();
+            $employee->first_name = $firstname;
+            $employee->last_name = $lastname;
+            $employee->email = $email;
+            $employee->phone = $phoneNumber;
+            $employee->date_of_birth = $dateOfBirth;
+            $employee->gender = $gender;
+            $employee->address = $address;
+            $employee->skills = $skill;
+            $employee->basic_salary = $sallary;
+            $employee->save();
+        }
+    }
 }
