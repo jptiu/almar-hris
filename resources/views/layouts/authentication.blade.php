3371e933533fca95cb89ee0045e25f255f3d4ae6
kentpaul <kentpaulmadrid28@gmail.com>
2024-10-02 22:36:04 +0800

    update img login

--- resources/views/layouts/authentication.blade.php
+++ resources/views/layouts/authentication.blade.php
@@ -59,7 +59,7 @@
 
                 <!-- Image -->
                 <div class="hidden md:block absolute top-0 bottom-0 right-0 md:w-1/2" aria-hidden="true">
-                    <img class="object-cover object-center w-full h-full" src="{{ asset('images/auth-image.jpg') }}" width="760" height="1024" alt="Authentication image" />
+                    <img class="object-cover object-center w-full h-full" src="{{ asset('images/loandes.svg') }}" width="760" height="1024" alt="Authentication image" />
                 </div>
 
             </div>
