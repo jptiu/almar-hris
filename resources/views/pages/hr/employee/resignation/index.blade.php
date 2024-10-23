b6f26d05bc94530f4815d2b829a8bee3a33d06cd
Christian James Simene <christianjamessimeneefiva@gmail.com>
2024-10-23 11:07:21 -0700

    add resignation crud wip

--- resources/views/pages/hr/employee/resignation/index.blade.php
+++ resources/views/pages/hr/employee/resignation/index.blade.php
@@ -33,6 +33,14 @@
                 <!-- Filter button -->
                 <x-dropdown-filter align="right" />
 
+                <a href="{{ route('resignation.add') }}" class="btn bg-indigo-500 hover:bg-indigo-600 text-white">
+                    <svg class="w-4 h-4 fill-current opacity-50 shrink-0" viewBox="0 0 16 16">
+                        <path
+                            d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
+                    </svg>
+                    <span class="xs:block ml-2">Add Resignee</span>
+                </a>
+
             </div>
 
         </div>
@@ -42,7 +50,7 @@
         <section class="container px-4 mx-auto">
             <div class="flex flex-col">
                 <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                    <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+                    <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-4">
                         <div class="overflow-hidden border border-gray-200 dark:border-gray-700 md:rounded-lg">
                             <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
                                 <thead class="bg-gray-50 dark:bg-gray-800">
